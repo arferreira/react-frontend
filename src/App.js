2930580bc19664be8ea3bebd463e2dddae92c3e4
Antonio Ricardo <antonioricardoarfs@gmail.com>
2019-10-05 12:31:35 -0400

    Styles to session view

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import './App.css';
+import logo from './assets/logo.svg';
 
 function App() {
-	return <h1>Olá Mundo</h1>;
+	return (
+		<div className="container">
+			<img src={logo} alt="AirCNC" />
+
+			<div className="content">
+				<p>
+					Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua
+					empresa
+				</p>
+
+				<form>
+					<label htmlFor="email">Email*</label>
+					<input type="email" id="email" placeholder="Seu melhor email" />
+					<button type="submit" className="btn">
+						Entrar
+					</button>
+				</form>
+			</div>
+		</div>
+	);
 }
 
 export default App;
